cc5e9e02e0158547d60132724680d1cc3342387d
songpeng22 <songpeng24@msn.com>
2021-12-28 14:12:01 +0800

    call jar file generated by other java project

---
+++ 05_java_module/src/com/android/javamodule/JavaModule.java
@@ -0,0 +1,48 @@
+package com.android.javamodule;
+//
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class JavaModule {
+	public static void appendLog(String text)
+	{       
+	   File logFile = new File("./log.txt");
+	   if (!logFile.exists())
+	   {
+	      try
+	      {
+	         logFile.createNewFile();
+	      } 
+	      catch (IOException e)
+	      {
+	         // TODO Auto-generated catch block
+	         e.printStackTrace();
+	      }
+	   }
+	   try
+	   {
+	      //BufferedWriter for performance, true to set append to file flag
+	      BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true)); 
+	      buf.append(text);
+	      buf.newLine();
+	      buf.close();
+	   }
+	   catch (IOException e)
+	   {
+	      // TODO Auto-generated catch block
+	      e.printStackTrace();
+	   }
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is JavaModule.");
+		appendLog("this is simple file log append.");
+	}
+
+}
---
+++ 06_call_java_module/src/com/android/javacaller/JavaCaller.java
@@ -0,0 +1,19 @@
+package com.android.javacaller;
+//
+import com.android.javamodule.JavaModule;
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class JavaCaller {
+	private static JavaModule javaModule;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		javaModule.main(args);
+	}
+
+}
